f1a43309414b93a4bfcb08ffcd19439b8855964c
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2024-07-11 14:26:11 +0400

    upd

--- src/Components/Details/Modal.jsx
+++ src/Components/Details/Modal.jsx
@@ -2,35 +2,33 @@ import React, { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 const Modal = ({ show, onClose, className, children }) => {
-  if (!show) {
+  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' });
+
+  if (!show || !isDesktopOrLaptop) {
     return null;
   }
-
-  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' });
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
-      onClose()
+      onClose();
       section.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
     <section>
-      {isDesktopOrLaptop && (
-            <div className={`modal-overlay ${className}`}>
-            <div className="modal">
-              <button className="close-button" onClick={onClose}>×</button>
-              <div className="modal-content">
-                {children}
-                <div className='modal-bottom-content'>
-                <button className='modal-contact-button' onClick={() => scrollToSection('contact-section')}>Contact Us</button>
-                </div>
-              </div>
+      <div className={`modal-overlay ${className}`}>
+        <div className="modal">
+          <button className="close-button" onClick={onClose}>×</button>
+          <div className="modal-content">
+            {children}
+            <div className='modal-bottom-content'>
+              <button className='modal-contact-button' onClick={() => scrollToSection('contact-section')}>Contact Us</button>
             </div>
           </div>
-      )}
+        </div>
+      </div>
     </section>
   );
 };
